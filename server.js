756c079e7a9beea91e60ab4a94278be09f431b99
Timothy Dry <timothy.dry@verizon.net>
2017-06-22 04:07:12 -0400

    turned off listen on port 3000

--- server.js
+++ server.js
@@ -48,5 +48,5 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 require('./src/app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+// app.listen(port);
+// console.log('The magic happens on port ' + port);
